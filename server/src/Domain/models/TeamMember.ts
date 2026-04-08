38481146dc7aa9ce2fc33298868eebc76c3a856c
nole78 <daki.nole@gmail.com>
2026-04-08 13:36:19 +0200

    Implemented TeamMember entity

--- server/src/Domain/models/TeamMember.ts
+++ server/src/Domain/models/TeamMember.ts
@@ -1,5 +1,11 @@
+import { TeamRole } from '../enums/TeamRole';
 //TODO: implement entity team_member
 
 export class TeamMember{
-    
+    constructor(
+        public teamId : number = 0,
+        public userId : number = 0,
+        public role : TeamRole = TeamRole.MEMBER,
+        public joinedAt : Date = new Date(),
+    ){}
 }
