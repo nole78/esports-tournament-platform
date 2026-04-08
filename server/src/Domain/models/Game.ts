4933e6410e8b6182fe3ec59d1c2b5aae5ddc84b6
nole78 <daki.nole@gmail.com>
2026-04-08 14:00:07 +0200

    Implemented Game entity

--- server/src/Domain/models/Game.ts
+++ server/src/Domain/models/Game.ts
@@ -1,5 +1,11 @@
 //TODO: implement entity game
 
 export class Game{
-    
+    constructor(
+        public gameId : number = 0,
+        public gameName : string = "",
+        public gameLogotip : string = "",
+        public gameGenre : string = "",
+        public gamePlayers: number = 0,
+    ){}
 }
