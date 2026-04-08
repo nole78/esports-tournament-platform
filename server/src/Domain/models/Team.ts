79f261f60aebe54c9546260784da7e908e4c58a7
nole78 <daki.nole@gmail.com>
2026-04-08 13:51:39 +0200

    Implemented team entity

--- server/src/Domain/models/Team.ts
+++ server/src/Domain/models/Team.ts
@@ -1,5 +1,11 @@
 // TODO: implement Team class
 
 export class Team{
-
+    constructor(
+        public teamId : number = 0,
+        public teamName : string = "",
+        public teamTag : string = "",
+        public teamLogotip : string = "",
+        public teamDescription : string = "",
+    ){}
 }
