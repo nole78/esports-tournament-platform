b038881d03b63162333d5086a26783ecb06083ab
nole78 <daki.nole@gmail.com>
2026-04-19 20:14:26 +0200

    added game catalog page skelet

---
+++ client/src/pages/common_pages/game_catalog.tsx
@@ -0,0 +1,13 @@
+import { PageHeader } from "../../components/ui/UI";
+
+
+export default function GameCatalog(){
+
+
+
+    return (
+        <div>
+            <PageHeader eyebrow="user" title="Game Catalog"/>
+        </div>
+    )
+}
