854c8a50212bbb0f78b9bae6bc922da21281ed8e
nole78 <daki.nole@gmail.com>
2026-04-19 20:13:57 +0200

    Added game catalog route for user and changed user dasboard route

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -4,7 +4,8 @@ import { useAuth } from "../../hooks/auth/useAuthHook";
 
 // TODO: Update nav items to match your routes and roles
 const userNav = [
-  { to: "/dashboard", label: "Dashboard", icon: "⬡" },
+  { to: "/player", label: "Dashboard", icon: "⬡" },
+  { to: "/player/game_catalog", label: "Game Catalog", icon: "⬡"},
   // add more user routes here
 ];
 const adminNav = [
