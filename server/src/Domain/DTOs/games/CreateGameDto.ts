52608af2b4d90491aebf37c6eb8f250c149589e1
nole78 <daki.nole@gmail.com>
2026-04-20 01:38:58 +0200

    Implemented CreateGameDto

--- server/src/Domain/DTOs/games/CreateGameDto.ts
+++ server/src/Domain/DTOs/games/CreateGameDto.ts
@@ -1,5 +1,10 @@
 // TODO: implement class for Game DTO creation
 
 export class CreateGameDto{
-
+    public constructor(
+        public name: string         = "",
+        public logo: string         = "",
+        public genre: string        = "",
+        public playerCnt: number    = 0
+    ){}
 }
