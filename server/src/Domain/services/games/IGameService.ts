25dc95231f36b8dd9248774a7b344212d6e11c17
nole78 <daki.nole@gmail.com>
2026-04-20 01:15:45 +0200

    Added interface GameService on server

---
+++ server/src/Domain/services/games/IGameService.ts
@@ -0,0 +1,12 @@
+import { CreateGameDto } from "../../DTOs/games/CreateGameDto";
+import { GameDto } from "../../DTOs/games/GameDto";
+import { PaginatedListDto } from "../../DTOs/PaginatedListDto";
+
+
+export interface IGameService{
+    gatAll(page?: number,limit?: number) : Promise<PaginatedListDto<GameDto>>
+    getById(id: number) : Promise<GameDto | null>
+    create(dto: CreateGameDto) : Promise<GameDto | null>
+    update(id: number, fields: Partial<GameDto>) : Promise<boolean>
+    delete(id: number) : Promise<boolean>
+}
