305674dc6372cc674ea56196d60dbe1b1ba8a0f2
David Novaković <daki.nole@gmail.com>
2026-04-28 20:56:02 +0200

    Removed unnecesary comments

--- client/src/types/audit/AuditList.ts
+++ client/src/types/audit/AuditList.ts
@@ -1,7 +1,10 @@
+// separate 
+// move into models
 export type PaginatedList<T> = {
   items: T[];
   total: number;
   page: number;
   pageSize: number;
 };
+// separate file
 export type ApiResponse<T> = { success: boolean; message?: string; data?: T };
