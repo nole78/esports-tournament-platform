89f3025498e52f6e7f4a46560af5bdd39fdabbf2
Danko Marković <dankomarkovic20042@gmail.com>
2026-04-30 22:29:15 +0200

    Updated app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,16 +8,21 @@ import { DbManager } from "./Database/connection/DbConnectionPool";
 import { UserRepository }   from "./Database/repositories/users/UserRepository";
 import { EntityRepository } from "./Database/repositories/entity/EntityRepository";
 import { GameRepository } from './Database/repositories/games/GameRepository';
+import { TournamentRepository } from "./Database/repositories/tournament/TournamentRepository";
 
 import { AuthService }   from "./Services/auth/AuthService";
 import { UserService }   from "./Services/users/UserService";
 import { EntityService } from "./Services/entity/EntityService";
 import { GameService } from './Services/games/GameService';
+import { TournamentService } from "./Services/tournaments/TournamentService";
 
 import { AuthController }   from "./WebAPI/controllers/AuthController";
 import { UserController }   from "./WebAPI/controllers/UserController";
 import { EntityController } from "./WebAPI/controllers/EntityController";
 import { GameController } from "./WebAPI/controllers/GameController";
+import { TournamentController } from "./WebAPI/controllers/TournamentController";
+
+
 
 export const logger = new ConsoleLoggerService();
 export const db     = new DbManager(logger);
@@ -26,12 +31,14 @@ export const db     = new DbManager(logger);
 const userRepo   = new UserRepository(db, logger);
 const entityRepo = new EntityRepository(db, logger);
 const gameRepo = new GameRepository(db, logger);
+const tournamentRepo = new TournamentRepository(db, logger);
 
 // Services
 const authService   = new AuthService(userRepo);
 const userService   = new UserService(userRepo);
 const entityService = new EntityService(entityRepo);
 const gameService   = new GameService(gameRepo);
+const tournamentService = new TournamentService(tournamentRepo, gameRepo, logger);
 
 // Express
 const app = express();
@@ -42,5 +49,6 @@ app.use("/api/v1", new AuthController(authService).getRouter());
 app.use("/api/v1", new UserController(userService).getRouter());
 app.use("/api/v1", new EntityController(entityService).getRouter());
 app.use("/api/v1", new GameController(gameService).getRouter());
+app.use("/api/v1", new TournamentController(tournamentService).getRouter());
 
 export default app;
