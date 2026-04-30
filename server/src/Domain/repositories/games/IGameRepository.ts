dab1605c9be8a5c738ba62fbbb7d2d7f0e9b3453
Danko Marković <dankomarkovic20042@gmail.com>
2026-04-30 19:28:51 +0200

    Added findByName method in gameRepository Interface

--- server/src/Domain/repositories/games/IGameRepository.ts
+++ server/src/Domain/repositories/games/IGameRepository.ts
@@ -5,6 +5,7 @@ import { Game } from "../../models/Game";
 
 export interface IGameRepository {
   findById(id: number): Promise<GameDto | null>;
+  findByName(name: string): Promise<GameDto | null>;
   findAll(page?: number, limit?: number): Promise<GameDto[]>;
   create(dto: CreateGameDto): Promise<Game>;
   update(id: number, fields: Partial<Game>): Promise<boolean>;
