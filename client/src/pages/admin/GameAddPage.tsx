396c9eeafc04be48337e3280bc086c838fb1fddc
David Novaković <daki.nole@gmail.com>
2026-05-06 15:13:45 +0200

    changed div to main tags

--- client/src/pages/admin/GameAddPage.tsx
+++ client/src/pages/admin/GameAddPage.tsx
@@ -5,8 +5,8 @@ import { gameApi } from '../../api_services/game_catalog/GameAPIService';
 
 export default function GameAddPage(){
     return (
-        <div className="min-h-screen bg-primary flex items-center justify-center px-4">
+        <main className="min-h-screen bg-primary flex items-center justify-center px-4">
             <GameAddForm gameApi={gameApi}/>
-        </div>
+        </main>
     );
 }
