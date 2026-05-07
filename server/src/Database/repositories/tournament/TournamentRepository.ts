e23801dfb817c27b5f3e9367841dc57f0c81ae3d
David Novaković <daki.nole@gmail.com>
2026-05-07 14:01:56 +0200

    put unimplemented code in comments so that back could be deployed

--- server/src/Database/repositories/tournament/TournamentRepository.ts
+++ server/src/Database/repositories/tournament/TournamentRepository.ts
@@ -1,4 +1,4 @@
-import { ITournamentRepository } from "../../../Domain/repositories/tournaments/ITournamentRepository";
+/*import { ITournamentRepository } from "../../../Domain/repositories/tournaments/ITournamentRepository";
 import { Tournament } from '../../../Domain/models/Tournament';
 import { ResultSetHeader, RowDataPacket } from "mysql2";
 import { DbManager } from "../../connection/DbConnectionPool";
@@ -90,4 +90,4 @@ export class TournamentRepository implements ITournamentRepository {
       return false;
     } finally { res.conn.release(); }
   }
-}
+}*/
