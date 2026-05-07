03540f6fd2ba0c76ea31809bef95ef0a63bd69b6
Danko Marković <dankomarkovic20042@gmail.com>
2026-05-08 00:11:33 +0200

    Added internal Dto with game id

---
+++ server/src/Domain/DTOs/tournaments/TournamentInternalDto.ts
@@ -0,0 +1,14 @@
+import { TournamentFormat } from "../../enums/TournamentFormat";
+import { TournamentStatus } from "../../enums/TournamentStatus";
+
+export class TournamentInternalDto{
+    constructor(
+            public tournamentName : string = "",
+            public tournamentGameId : number = 0,
+            public tournamentFormat : TournamentFormat = TournamentFormat.SINGLE_ELIMINATION,
+            public tournamentMaxTeams : number = 0,
+            public tournamentApplicationDeadline : Date = new Date(),
+            public tournamentPrizeFund : number = 0,
+            public torunamentStatus : TournamentStatus = TournamentStatus.UPCOMING,
+        ){}
+}
