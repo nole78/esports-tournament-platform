f0fb2bc854e32856fa766252570350a50b601c2f
David Novaković <daki.nole@gmail.com>
2026-05-07 20:12:47 +0200

    changed api service to match model names

--- client/src/api_services/auth/IAuthAPIService.ts
+++ client/src/api_services/auth/IAuthAPIService.ts
@@ -2,5 +2,5 @@ import type { AuthResponse } from "../../types/auth/AuthResponse";
 
 export interface IAuthAPIService {
   login(username: string, password: string): Promise<AuthResponse>;
-  register(username: string, email: string, password: string, fullName: string, picture: string,role: string): Promise<AuthResponse>;
+  register(username: string, email: string, password: string, fullName: string, profilePicture: string,role: string): Promise<AuthResponse>;
 }
