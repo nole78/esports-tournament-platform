15daac976b8221b1d51bb44eed931b8bab5b20f7
David Novaković <daki.nole@gmail.com>
2026-05-09 02:37:49 +0200

    added update password to user service

--- server/src/Domain/services/users/IUserService.ts
+++ server/src/Domain/services/users/IUserService.ts
@@ -1,6 +1,7 @@
 
 import { UserDto } from "../../DTOs/users/UserDto";
 import { UserInfoDto } from "../../DTOs/users/UserInfoDto";
+import { UserPasswordDto } from "../../DTOs/users/UserPasswordDto";
 import { User } from "../../models/User";
 
 export interface IUserService {
@@ -9,4 +10,5 @@ export interface IUserService {
   deactivate(id: number): Promise<boolean>;
   getInfo(id: number): Promise<UserInfoDto | null>
   update(id: number, fields: Partial<User>) : Promise<boolean>;
+  updatePassword(id: number, payload: UserPasswordDto) : Promise<boolean>;
 }
--- server/src/Services/users/UserService.ts
+++ server/src/Services/users/UserService.ts
@@ -4,6 +4,7 @@ import { UserDto } from "../../Domain/DTOs/users/UserDto";
 import { UserInfoDto } from "../../Domain/DTOs/users/UserInfoDto";
 import { User } from "../../Domain/models/User";
 import bcrypt from "bcryptjs";
+import { UserPasswordDto } from "../../Domain/DTOs/users/UserPasswordDto";
 
 export class UserService implements IUserService {
   private readonly saltRounds = parseInt(process.env.SALT_ROUNDS ?? "10", 10);
@@ -27,7 +28,7 @@ export class UserService implements IUserService {
   async getInfo(id: number): Promise<UserInfoDto | null> {
     const u = await this.userRepo.findById(id);
     if (u.id === 0) return null;
-    return new UserInfoDto(u.gamerTag,u.email,u.fullName,u.passwordHash,u.profilePicture);
+    return new UserInfoDto(u.gamerTag,u.email,u.fullName,u.passwordHash);
   }
 
   async update(id:number, fields: Partial<UserInfoDto>) : Promise<boolean>{
@@ -35,24 +36,26 @@ export class UserService implements IUserService {
     if(fields.gamerTag)
     {
       const byName = await this.userRepo.findByUsername(fields.gamerTag);
-      if (byName.id == id) {
-        return false;
-      }
+      if (byName.id == id) return false;
     }
     if(fields.email)
     {
       const byEmail = await this.userRepo.findByEmail(fields.email);
-      if (byEmail.id == id) 
-      {
-        return false;
-      }
-    }
-    if(fields.password)
-    {
-      const hash = await bcrypt.hash(fields.password, this.saltRounds).catch(() => "");
-        if (!hash) return false;
-      return await this.userRepo.update(id,{gamerTag: fields.gamerTag,email: fields.email,fullName: fields.fullName,profilePicture: fields.profilePicture, passwordHash: hash})
+      if (byEmail.id == id) return false;
     }
     return await this.userRepo.update(id,{gamerTag: fields.gamerTag,email: fields.email,fullName: fields.fullName,profilePicture: fields.profilePicture})
+  }
+
+  async updatePassword(id: number, payload: UserPasswordDto) : Promise<boolean>{
+    const byId = await this.userRepo.findById(id);
+    if(byId.id === 0) return false;
+
+    const match = await bcrypt.compare(payload.oldPassword, byId.passwordHash).catch(() => false);
+    if(!match) return false;
+
+    const hash = await bcrypt.hash(payload.newPassword, this.saltRounds).catch(() => "");
+    if (!hash) return false;
+
+    return await this.userRepo.update(id,{passwordHash: hash});
   }
 }
