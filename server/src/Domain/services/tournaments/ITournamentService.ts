c3a15c8a86f5473ca320bae9b6a26d57566563dc
Danko Marković <dankomarkovic20042@gmail.com>
2026-05-10 23:17:36 +0200

    Added method for filtering in service interface

--- server/src/Domain/services/tournaments/ITournamentService.ts
+++ server/src/Domain/services/tournaments/ITournamentService.ts
@@ -2,11 +2,13 @@ import { CreateTournamentDto } from "../../DTOs/tournaments/CreateTournamentDto"
 import { TournamentDto } from "../../DTOs/tournaments/TorunamentDto";
 import { PaginatedListDto } from "../../DTOs/PaginatedListDto";
 import { Tournament } from "../../models/Tournament";
+import { TournamentFilterDto } from "../../DTOs/tournaments/TournamentFilterDto";
 
 export interface ITournamentService {
   getAll(page?: number, limit?: number): Promise<PaginatedListDto<TournamentDto>>;
   getById(id: number): Promise<TournamentDto | null>;
   create(dto: CreateTournamentDto): Promise<Tournament | null>;
+  getFiltered(fields: Partial<TournamentFilterDto>, page?:number, limit?: number): Promise<PaginatedListDto<TournamentDto>>;
   update(id: number, fields: Partial<Tournament>): Promise<boolean>;
   delete(id: number): Promise<boolean>;
 }
