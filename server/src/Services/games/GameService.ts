371c7e774bb428e1d7b7d3d7f79a35807986646e
David Novaković <daki.nole@gmail.com>
2026-05-12 19:29:20 +0200

    Updated GameService to  create DTOs (map) from models returnd by repo

--- server/src/Services/games/GameService.ts
+++ server/src/Services/games/GameService.ts
@@ -1,23 +1,34 @@
 import { CreateGameDto } from '../../Domain/DTOs/games/CreateGameDto';
 import { GameDto } from '../../Domain/DTOs/games/GameDto';
 import { PaginatedListDto } from '../../Domain/DTOs/PaginatedListDto';
+import { Game } from '../../Domain/models/Game';
 import { IGameRepository } from '../../Domain/repositories/games/IGameRepository';
 import { IGameService } from '../../Domain/services/games/IGameService';
 
 export class GameService implements IGameService {
     public constructor(private readonly gameRepo : IGameRepository){}
 
+    private toGameDto(game: Game) : GameDto{
+        return new GameDto(game.gameId,game.gameName,game.gameLogotip,game.gameGenre,game.gamePlayers)
+    }
+
     async getAll(page?: number,limit?: number) : Promise<PaginatedListDto<GameDto>>{
         const items = await this.gameRepo.findAll(page, limit);
-        return items;
+        const total = await this.gameRepo.getTotal();
+        const list = items.map(i => this.toGameDto(i))
+        return new PaginatedListDto( list, total, page ?? 1, limit ?? 20);
     }
     async getById(id: number) : Promise<GameDto | null>{
-        return this.gameRepo.findById(id);
+        const item = await this.gameRepo.findById(id);
+        if(item.gameId === 0)
+            return null;
+        return this.toGameDto(item);
     }
     async create(dto: CreateGameDto) : Promise<GameDto | null>{
         const created = await this.gameRepo.create(dto);
-        if(created.gameId === 0) return null;
-        return new GameDto(created.gameId, created.gameName, created.gameLogotip, created.gameGenre, created.gamePlayers);
+        if(created.gameId === 0) 
+            return null;
+        return this.toGameDto(created);
     }
     async update(id: number, fields: Partial<GameDto>) : Promise<boolean>{
         return this.gameRepo.update(id, fields);
