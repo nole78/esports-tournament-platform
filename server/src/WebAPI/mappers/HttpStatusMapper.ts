dd5a9f08f71045aa2c6b7e888cdbe314b3d2600e
David Novaković <daki.nole@gmail.com>
2026-05-15 21:50:38 +0200

    added error type to http status mapper

---
+++ server/src/WebAPI/mappers/HttpStatusMapper.ts
@@ -0,0 +1,19 @@
+import { ErrorType } from "../../Domain/common/ErrorType";
+
+export function mapErrorTypeToStatus(errorType: ErrorType | null): number {
+
+    switch (errorType) {
+
+        case ErrorType.NotFound:
+            return 404;
+
+        case ErrorType.Conflict:
+            return 409;
+
+        case ErrorType.Validation:
+            return 400;
+
+        default:
+            return 500;
+    }
+}
