4efe7e1ed76a4b651b3c65517531dd052de46773
David Novaković <daki.nole@gmail.com>
2026-05-15 22:47:03 +0200

    Fixed Result pattern to not have any or undefined

--- server/src/Domain/common/Result.ts
+++ server/src/Domain/common/Result.ts
@@ -1,31 +1,26 @@
-import { ErrorType } from "./ErrorType";
+import { ErrorType } from './ErrorType';
 
-type Success<T> = {
-    isSuccess: true;
-    value: T;
-};
+export class Result<T = void>{
+    public isSuccess: boolean = false;
+    public errorMessage: string | null = null;
+    public errorType: ErrorType | null = null;
+    public value: T | null = null;
 
-type Failure = {
-    isSuccess: false;
-    errorMessage: string;
-    errorType: ErrorType;
-};
+    protected constructor(isSuccess: boolean, value: T | null, errorMessage: string | null, errorType: ErrorType | null)
+    {
+        this.isSuccess = isSuccess;
+        this.value = value;
+        this.errorMessage = errorMessage;
+        this.errorType = errorType;
+    } 
 
-export type Result<T> = Success<T> | Failure;
-
-export const Result = {
-    success<T>(value: T): Result<T> {
-        return {
-            isSuccess: true,
-            value
-        };
-    },
-
-    failure<T>(errorMessage: string, errorType: ErrorType): Result<T> {
-        return {
-            isSuccess: false,
-            errorMessage,
-            errorType
-        };
+    public static Success(): Result<void>;
+    public static Success<T>(value: T): Result<T>;
+    public static Success<T>(value?: T) : Result<T | void>{
+        return new Result<T | void>(true,value ?? null,null,null)
     }
-};
+
+    public static Failiure<T>(errorMessage: string, errorType: ErrorType) : Result<T>{
+        return new Result<T>(false,null,errorMessage,errorType);
+    }
+}
