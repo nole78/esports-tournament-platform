d93b129fbaecee960dffa92f4b65912aa5e9c654
David Novaković <daki.nole@gmail.com>
2026-05-16 15:21:21 +0200

    Implemented index.ts

---
+++ loadbalancer/src/index.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import app, {logger} from "./app"
+import { loadBalancerConfig } from './config/loadBalancerConfig';
+
+const PORT = loadBalancerConfig.port;
+
+async function start(): Promise<void> {
+
+  app.listen(PORT, () => {
+    logger.info("Server", `Running on port: ${PORT}`);
+  });
+}
+
+start().catch((err) => logger.error("Load balancer", "Fatal startup error", err));
