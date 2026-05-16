187bd97da7e18291174f7ba3e4813582bad8bf36
David Novaković <daki.nole@gmail.com>
2026-05-16 18:51:13 +0200

    added healthcheck to index.ts

--- loadbalancer/src/index.ts
+++ loadbalancer/src/index.ts
@@ -1,13 +1,16 @@
 import 'dotenv/config';
 import app, {logger} from "./app"
 import { loadBalancerConfig } from './config/loadBalancerConfig';
+import { startHealthCheck } from './services/healthCheckService';
 
 const PORT = loadBalancerConfig.port;
 
 async function start(): Promise<void> {
   app.listen(PORT, () => {
-    logger.info("Server", `Running on port: ${PORT}`);
+    logger.info("LB", `Running on port: ${PORT}`);
   });
+
+  startHealthCheck();
 }
 
 start().catch((err) => logger.error("Load balancer", "Fatal startup error", err));
