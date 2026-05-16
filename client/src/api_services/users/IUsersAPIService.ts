a4e82a9a27ab4e3c1af0d37580f93f969e055c8c
David Novaković <daki.nole@gmail.com>
2026-05-16 02:27:02 +0200

    Fixed not removed method in api service

--- client/src/api_services/users/IUsersAPIService.ts
+++ client/src/api_services/users/IUsersAPIService.ts
@@ -1,4 +1,3 @@
-import type { UserInfoDto } from "../../models/user/UserInfoDto";
 import type { UserDto } from "../../models/user/UserTypes";
 
 export type ApiResponse<T> = { success: boolean; message: string; data?: T };
@@ -7,5 +6,4 @@ export interface IUsersAPIService {
   getAll(): Promise<ApiResponse<UserDto[]>>;
   getById(id: number): Promise<ApiResponse<UserDto>>;
   changeRole(id: number,role: string): Promise<ApiResponse<void>>;
-  getInfo(): Promise<ApiResponse<UserInfoDto>>;
 }
