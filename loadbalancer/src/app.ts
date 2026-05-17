9bebd063290fa48cf8dd214aa56662a726391caa
David Novaković <daki.nole@gmail.com>
2026-05-17 19:11:00 +0200

    added cors to loadbalancer so that health check can run

--- loadbalancer/src/app.ts
+++ loadbalancer/src/app.ts
@@ -5,6 +5,7 @@ import { ServerPoolService } from './services/ServerPoolService';
 import { createStrategy } from './factories/createStrategy';
 import { HealthCheckService } from './services/HealthCheckService';
 import { createHealthRouter } from './routes/healthRoutes';
+import cors from 'cors';
 
 const app = express();
 
@@ -14,6 +15,12 @@ const serverPool = new ServerPoolService(strategy);
 export const logger = new ConsoleLoggerService();
 export const healthCheck = new HealthCheckService(logger);
 
+app.use(cors());
+
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
 app.use(createHealthRouter(healthCheck));
 app.use(proxyMiddleware(serverPool));
 
