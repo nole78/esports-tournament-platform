02b1286080c9eaa0e041dcade278b8fd794b7c0c
David Novaković <daki.nole@gmail.com>
2026-05-17 03:03:58 +0200

    fixed load balancer config

--- loadbalancer/src/config/loadBalancerConfig.ts
+++ loadbalancer/src/config/loadBalancerConfig.ts
@@ -1,7 +1,18 @@
 import { LoadBalancingAlgorithm } from '../types/LoadBalancingAlgorithm';
+import dotenv from 'dotenv';
 
-const algorithm = process.env.LB_ALGORITHM as LoadBalancingAlgorithm ?? LoadBalancingAlgorithm.ROUND_ROBIN;
+dotenv.config();
+
+const algorithm = Object.values(LoadBalancingAlgorithm)
+    .includes(process.env.LB_ALGORITHM as LoadBalancingAlgorithm)
+    ? (process.env.LB_ALGORITHM as LoadBalancingAlgorithm)
+    : LoadBalancingAlgorithm.ROUND_ROBIN;
+
 const port = parseInt(process.env.LB_PORT ?? "8080", 10);
+
+const healthCheckInterval = parseInt(process.env.HEALTH_CHECK_INTERVAL ?? "10000", 10);
+
+const healthCheckTimeout = parseInt(process.env.HEALTH_CHECK_TIMEOUT ?? "3000", 10);
 
 export const loadBalancerConfig = {
 
@@ -9,7 +20,7 @@ export const loadBalancerConfig = {
 
     port,
 
-    healthCheckInterval: Number(
-        process.env.HEALTH_CHECK_INTERVAL
-    )
+    healthCheckInterval,
+
+    healthCheckTimeout
 };
