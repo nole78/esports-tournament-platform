90d71a3d28b3b55f01344f86b95e577baa0873c7
David Novaković <daki.nole@gmail.com>
2026-05-17 19:13:02 +0200

    Changed serverStatusDto to class

--- loadbalancer/src/Domain/DTOs/ServerStatusDto.ts
+++ loadbalancer/src/Domain/DTOs/ServerStatusDto.ts
@@ -1,7 +1,11 @@
 import { ServerStatus } from "../enums/ServerStatus";
 
-export interface ServerStatusDto {
-    id: string;
-    status: ServerStatus;
-    latency: number;
+export class ServerStatusDto {
+    constructor(
+    public name: string = "",
+    public url: string = "",
+    public status: ServerStatus = ServerStatus.UNREACHABLE,
+    public lastCheck: Date | null = null,
+    public latency: number = 0,
+    ){}
 }
