6ebb7c5555732e1359bdc09cd75aa1d3ba3bda8e
David Novaković <daki.nole@gmail.com>
2026-05-17 13:55:05 +0200

    implemented create strategy function

--- loadbalancer/src/factories/createStrategy.ts
+++ loadbalancer/src/factories/createStrategy.ts
@@ -3,6 +3,8 @@ import { LeastConnectionsStrategy } from "../algorithms/LeastConnectionsStrategy
 import { RoundRobinStrategy } from "../algorithms/RoundRobinStrategy";
 import { loadBalancerConfig } from "../config/loadBalancerConfig";
 import { LoadBalancingAlgorithm } from "../domain/enums/LoadBalancingAlgorithm";
+import { WeightedRoundRobinStrategy } from "../algorithms/WeightedRoundRobinStrategy";
+import { IpHashStrategy } from "../algorithms/IpHashStrategy";
 
 export function createStrategy(): ILoadBalancingStrategy {
 
@@ -13,6 +15,12 @@ export function createStrategy(): ILoadBalancingStrategy {
 
         case LoadBalancingAlgorithm.LEAST_CONNECTIONS:
             return new LeastConnectionsStrategy();
+
+        case LoadBalancingAlgorithm.WEIGHTED_ROUND_ROBIN:
+            return new WeightedRoundRobinStrategy();
+
+        case LoadBalancingAlgorithm.IP_HASH:
+            return new IpHashStrategy();
 
         default:
             return new RoundRobinStrategy();
