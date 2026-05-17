fb6c1343a3d5dcd226346e8d4c51dd1a973f1fca
David Novaković <daki.nole@gmail.com>
2026-05-17 02:16:54 +0200

    changed health check service to be a class

--- loadbalancer/src/services/healthCheckService.ts
+++ loadbalancer/src/services/healthCheckService.ts
@@ -1,29 +1,36 @@
 import { servers } from "../config/servers";
 import { ServerInstance } from "../types/ServerInstance";
+import { ILoggerService } from '../utils/ILoggerService';
 
-export async function startHealthCheck(interval = 5000) {
+const HEALTH_CHECK_INTERVAL = parseInt(process.env.HEALTH_CHECK_INTERVAL ?? "10000", 10);
+const HEALTH_CHECK_TIMEOUT = parseInt(process.env.HEALTH_CHECK_TIMEOUT ?? "3000", 10);
+
+export class HealthCheckService {
+    public constructor(private readonly logger: ILoggerService){}
+
+    public  async start() {
     setInterval(async () => {
         for (const server of servers) {
-            const alive = await checkServer(server);
-
+            const alive = await this.checkServer(server);
             server.alive = alive;
-
-            if (!alive) {
-                console.log(`[LB] Server down: ${server.id}`);
-            }
+            if(!alive)
+                this.logger.warn("LB", `Server ${server.id} failed health check`);
         }
-    }, interval);
-}
 
-async function checkServer(server: ServerInstance): Promise<boolean> {
-    try {
-        const res = await fetch(`${server.url}/health`, {
-            method: "GET",
-            signal: AbortSignal.timeout(2000)
-        });
+        this.logger.info("LB",servers.map((s) => `${s.id}=${s.alive?"healthy":"unreachable"}`).join(" | "));
+    }, HEALTH_CHECK_INTERVAL);
+    }
 
-        return res.ok;
-    } catch {
-        return false;
+    private async checkServer(server: ServerInstance): Promise<boolean> {
+        try {
+            const res = await fetch(`${server.url}/api/v1/health`, {
+                method: "GET",
+                signal: AbortSignal.timeout(HEALTH_CHECK_TIMEOUT)
+            });
+
+            return res.ok;
+        } catch {
+            return false;
+        }
     }
 }
