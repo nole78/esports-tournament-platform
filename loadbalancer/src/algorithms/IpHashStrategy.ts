f27365d2f36e9bd994036fe21b037993d65a99b9
David Novaković <daki.nole@gmail.com>
2026-05-17 15:18:24 +0200

    implemented ip hash strategy

---
+++ loadbalancer/src/algorithms/IpHashStrategy.ts
@@ -0,0 +1,21 @@
+import { ILoadBalancingStrategy } from "../domain/interfaces/ILoadbalancingStrategy";
+import { ServerInstance } from "../domain/models/ServerInstance";
+import crypto from 'crypto';
+
+export class IpHashStrategy implements ILoadBalancingStrategy{
+    public getNextServer(servers: ServerInstance[], clientIp?: string): ServerInstance | null {
+        if(!clientIp || servers.length === 0) return null;
+        const hash = this.hashIp(clientIp);
+        const index = hash % servers.length;
+        
+        return servers[index];
+    }
+
+    private hashIp(ip: string): number {
+        return crypto
+            .createHash('md5')
+            .update(ip)
+            .digest()
+            .readUInt32BE(0);
+    }
+}
