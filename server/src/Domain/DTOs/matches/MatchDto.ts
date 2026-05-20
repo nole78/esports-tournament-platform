421b0ade939dee7b8f5b192303eff033216130c8
David Novaković <daki.nole@gmail.com>
2026-05-20 15:26:24 +0200

    Updated MatchDto

--- server/src/Domain/DTOs/matches/MatchDto.ts
+++ server/src/Domain/DTOs/matches/MatchDto.ts
@@ -4,9 +4,11 @@ import { MatchStatus } from "../../enums/MatchStatus";
 
 export class MatchDto{
     constructor(
-            public matchId : number = 0,
-            public matchResult : string = "",
-            public status : MatchStatus.SCHEDULED,
-            public matchRound : MatchRound = MatchRound.ROUND_OF_16,
+        public matchId : number = 0,
+        public blueTeamId : number = 0,
+        public redTeamId : number = 0,
+        public matchResult : string = "",
+        public status : MatchStatus = MatchStatus.SCHEDULED,
+        public matchRound : MatchRound = MatchRound.ROUND_OF_16,
     ){}
 }
