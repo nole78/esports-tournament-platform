b04a281ca970a7a3be271fba6043d7a34f40daf1
David Novaković <daki.nole@gmail.com>
2026-05-20 15:27:51 +0200

    Deined Match Service

---
+++ server/src/Domain/services/matches/IMatchService.ts
@@ -0,0 +1,13 @@
+import { Result } from "../../common/Result";
+import { CreateMatchDto } from "../../DTOs/matches/CreateMatchDto";
+import { MatchDto } from "../../DTOs/matches/MatchDto";
+import { PaginatedListDto } from "../../DTOs/PaginatedListDto";
+
+export interface IMatchService{
+    getAll(page?: number, limit?: number): Promise<Result<PaginatedListDto<MatchDto>>>;
+    getById(id: number): Promise<Result<MatchDto>>;
+    getByTeamId(teamId: number): Promise<Result<MatchDto[]>>;
+    create(dto: CreateMatchDto): Promise<Result<MatchDto>>;
+    update(id: number, fields: Partial<MatchDto>): Promise<Result<void>>;
+    delete(id: number): Promise<Result<void>>;
+}
