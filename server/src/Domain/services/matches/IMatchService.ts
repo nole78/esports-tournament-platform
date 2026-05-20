def1b8bbd9f82deaeb9baa113cdc1474a4296166
David Novaković <daki.nole@gmail.com>
2026-05-20 20:00:54 +0200

    added get by tournment id definiton

--- server/src/Domain/services/matches/IMatchService.ts
+++ server/src/Domain/services/matches/IMatchService.ts
@@ -7,6 +7,7 @@ export interface IMatchService{
     getAll(page?: number, limit?: number): Promise<Result<PaginatedListDto<MatchDto>>>;
     getById(id: number): Promise<Result<MatchDto>>;
     getByTeamId(teamId: number): Promise<Result<MatchDto[]>>;
+    getByTournamentId(tournamentId: number): Promise<Result<MatchDto[]>>;
     create(dto: CreateMatchDto): Promise<Result<MatchDto>>;
     update(id: number, fields: Partial<MatchDto>): Promise<Result<void>>;
     delete(id: number): Promise<Result<void>>;
