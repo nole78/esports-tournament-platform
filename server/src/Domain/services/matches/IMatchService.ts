ec9c2cdef8dc6fd0ac90889c133c91a3d7c9662e
David Novaković <daki.nole@gmail.com>
2026-05-22 00:46:14 +0200

    Added match service definition

--- server/src/Domain/services/matches/IMatchService.ts
+++ server/src/Domain/services/matches/IMatchService.ts
@@ -2,13 +2,18 @@ import { Result } from "../../common/Result";
 import { CreateMatchDto } from "../../DTOs/matches/CreateMatchDto";
 import { MatchDto } from "../../DTOs/matches/MatchDto";
 import { PaginatedListDto } from "../../DTOs/PaginatedListDto";
+import { MatchResultDto } from '../../DTOs/matches/MatchResultDto';
+import { UserDto } from "../../DTOs/users/UserDto";
 
 export interface IMatchService{
     getAll(page?: number, limit?: number): Promise<Result<PaginatedListDto<MatchDto>>>;
+    getAllPlayers(id: number): Promise<Result<UserDto>>;
     getById(id: number): Promise<Result<MatchDto>>;
     getByTeamId(teamId: number): Promise<Result<MatchDto[]>>;
     getByTournamentId(tournamentId: number): Promise<Result<MatchDto[]>>;
     create(dto: CreateMatchDto): Promise<Result<MatchDto>>;
-    update(id: number, fields: Partial<MatchDto>): Promise<Result<void>>;
+    setResult(id: number, result: MatchResultDto) : Promise<Result<void>>;
+    addPlayerToMatch(id: number, userId: number) :Promise<Result<void>>;
+    removePlayerFromMatch(id: number, userId: number) : Promise<Result<void>>;    
     delete(id: number): Promise<Result<void>>;
 }
