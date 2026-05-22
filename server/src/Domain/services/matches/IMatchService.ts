c776fce906d8804958d2591d1efe991e33e88d3d
David Novaković <daki.nole@gmail.com>
2026-05-23 00:51:36 +0200

    redefined methods to match controller routes and new dto

--- server/src/Domain/services/matches/IMatchService.ts
+++ server/src/Domain/services/matches/IMatchService.ts
@@ -4,6 +4,7 @@ import { MatchDto } from "../../DTOs/matches/MatchDto";
 import { PaginatedListDto } from "../../DTOs/PaginatedListDto";
 import { MatchResultDto } from '../../DTOs/matches/MatchResultDto';
 import { UserDto } from "../../DTOs/users/UserDto";
+import { AddPlayersDto } from "../../DTOs/match_players/AddPlayersDto";
 
 export interface IMatchService{
     getAll(page?: number, limit?: number): Promise<Result<PaginatedListDto<MatchDto>>>;
@@ -13,7 +14,8 @@ export interface IMatchService{
     getByTournamentId(tournamentId: number): Promise<Result<MatchDto[]>>;
     create(dto: CreateMatchDto): Promise<Result<MatchDto>>;
     setResult(id: number, result: MatchResultDto) : Promise<Result<void>>;
-    addPlayerToMatch(id: number, userId: number) :Promise<Result<void>>;
+    setPerformanceNotes(id: number, userId: number, notes: string) : Promise<Result<void>>;
+    addPlayersToMatch(id: number, dto: AddPlayersDto) :Promise<Result<void>>;
     removePlayerFromMatch(id: number, userId: number) : Promise<Result<void>>;    
     delete(id: number): Promise<Result<void>>;
 }
