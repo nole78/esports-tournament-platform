5827498ca5e8acca5def4e7348a3e06760332291
David Novaković <daki.nole@gmail.com>
2026-05-23 01:51:37 +0200

    Added match player dto

---
+++ server/src/Domain/DTOs/match_players/MatchPlayerDto.ts
@@ -0,0 +1,9 @@
+
+export class MatchPlayerDto{
+    constructor(
+        public userId : number = 0,
+        public teamId : number = 0,
+        public matchId : number = 0,
+        public performaceNotes : string = "",
+    ) {}
+}
