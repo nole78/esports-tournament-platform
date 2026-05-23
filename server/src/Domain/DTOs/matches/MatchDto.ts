d06bb24a777214d20f5973d404e4eb950056c3a0
David Novaković <daki.nole@gmail.com>
2026-05-24 00:10:38 +0200

    Updated MatchDto to match new model

--- server/src/Domain/DTOs/matches/MatchDto.ts
+++ server/src/Domain/DTOs/matches/MatchDto.ts
@@ -1,5 +1,5 @@
 // Should be allright, need to check
-import { MatchRound } from "../../enums/MatchRound";
+import { MatchSlot } from "../../enums/MatchSlot";
 import { MatchStatus } from "../../enums/MatchStatus";
 
 export class MatchDto{
@@ -8,8 +8,14 @@ export class MatchDto{
         public tournamentId: number = 0,
         public blueTeamId : number = 0,
         public redTeamId : number = 0,
-        public matchResult : string = "",
+        public winnerTeamId : number  = 0,
         public status : MatchStatus = MatchStatus.SCHEDULED,
-        public matchRound : MatchRound = MatchRound.ROUND_OF_16,
+        public roundNumber : number = 0,
+        public blueTeamScore : number = 0,
+        public redTeamScore : number = 0,
+        public WinnerToMatchId : number = 0,
+        public WinnerToSlot : MatchSlot = MatchSlot.BLUE,
+        public LoserToMatchId : number = 0,
+        public LoserToSlot : MatchSlot = MatchSlot.BLUE,
     ){}
 }
