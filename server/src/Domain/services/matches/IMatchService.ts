ffba95126c317334344c5d50466d81295e17e338
David Novaković <daki.nole@gmail.com>
2026-05-24 00:09:33 +0200

    Updated service definition to only needed methods

--- server/src/Domain/services/matches/IMatchService.ts
+++ server/src/Domain/services/matches/IMatchService.ts
@@ -1,10 +1,10 @@
 import { Result } from "../../common/Result";
-import { CreateMatchDto } from "../../DTOs/matches/CreateMatchDto";
 import { MatchDto } from "../../DTOs/matches/MatchDto";
 import { PaginatedListDto } from "../../DTOs/PaginatedListDto";
 import { MatchResultDto } from '../../DTOs/matches/MatchResultDto';
 import { UserDto } from "../../DTOs/users/UserDto";
 import { AddPlayersDto } from "../../DTOs/match_players/AddPlayersDto";
+import { AddPlayersResponseDto } from "../../DTOs/match_players/AddPlayersResponseDto";
 
 export interface IMatchService{
     getAll(page?: number, limit?: number): Promise<Result<PaginatedListDto<MatchDto>>>;
@@ -12,10 +12,8 @@ export interface IMatchService{
     getById(id: number): Promise<Result<MatchDto>>;
     getByTeamId(teamId: number): Promise<Result<MatchDto[]>>;
     getByTournamentId(tournamentId: number): Promise<Result<MatchDto[]>>;
-    create(dto: CreateMatchDto): Promise<Result<MatchDto>>;
     setResult(id: number, result: MatchResultDto) : Promise<Result<void>>;
     setPerformanceNotes(id: number, userId: number, notes: string) : Promise<Result<void>>;
-    addPlayersToMatch(id: number, dto: AddPlayersDto) :Promise<Result<void>>;
+    addPlayersToMatch(id: number, dto: AddPlayersDto) :Promise<Result<AddPlayersResponseDto>>;
     removePlayerFromMatch(id: number, userId: number) : Promise<Result<void>>;    
-    delete(id: number): Promise<Result<void>>;
 }
