c49ddfc04932fd442f978948c907624d81333393
David Novaković <daki.nole@gmail.com>
2026-05-24 21:21:53 +0200

    Expanded MatchDto

--- server/src/Domain/DTOs/matches/MatchDto.ts
+++ server/src/Domain/DTOs/matches/MatchDto.ts
@@ -8,12 +8,18 @@ export class MatchDto{
         public matchId : number = 0,
         public tournamentId: number = 0,
         public blueTeamId : number = 0,
+        public blueTeamName : string = "",
         public redTeamId : number = 0,
+        public redTeamName : string = "",
         public winnerTeamId : number  = 0,
         public status : MatchStatus = MatchStatus.SCHEDULED,
         public roundNumber : number = 0,
         public bracketType : BracketType = BracketType.WINNER, 
         public blueTeamScore : number = 0,
         public redTeamScore : number = 0,
+        public winnerToMatchId : number = 0,
+        public winnerToSlot : MatchSlot = MatchSlot.BLUE,
+        public loserToMatchId : number = 0,
+        public loserToSlot : MatchSlot = MatchSlot.BLUE,
     ){}
 }
