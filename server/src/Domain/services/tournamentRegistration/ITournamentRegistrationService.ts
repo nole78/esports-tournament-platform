50a7f9da81dcbbc9d6b36e518bbc1f8613a7cad9
Danko Marković <dankomarkovic20042@gmail.com>
2026-05-24 16:30:31 +0200

    Fixed for Result pattern

--- server/src/Domain/services/tournamentRegistration/ITournamentRegistrationService.ts
+++ server/src/Domain/services/tournamentRegistration/ITournamentRegistrationService.ts
@@ -1,11 +1,12 @@
 import { PaginatedListDto } from "../../DTOs/PaginatedListDto";
 import { CreateTournamentRegistrationDto } from "../../DTOs/tournament_registrations/CreateTournamentRegistrationDto";
 import { TournamentRegistrationDto } from "../../DTOs/tournament_registrations/TournamentRegistrationDto";
+import { Result } from '../../common/Result';
 
 export interface ITournamentRegistrationService{
-    getByTeamId(teamId: number, page?:number, limit?:number): Promise<PaginatedListDto<TournamentRegistrationDto>>;
-    getByTournamentId(tournamentId: number, page?:number, limit?:number): Promise<PaginatedListDto<TournamentRegistrationDto>>;
-    create(tr: CreateTournamentRegistrationDto): Promise<TournamentRegistrationDto | null>;
-    update(tournamentId: number, teamId: number, fields: Partial<TournamentRegistrationDto>): Promise<boolean>;
-    delete(tournamentId: number, teamId: number): Promise<boolean>;
+    getByTeamId(teamId: number, page?:number, limit?:number): Promise<Result<PaginatedListDto<TournamentRegistrationDto>>>;
+    getByTournamentId(tournamentId: number, page?:number, limit?:number): Promise<Result<PaginatedListDto<TournamentRegistrationDto>>>;
+    create(tr: CreateTournamentRegistrationDto): Promise<Result<TournamentRegistrationDto>>;
+    update(tournamentId: number, teamId: number, fields: Partial<TournamentRegistrationDto>): Promise<Result<void>>;
+    delete(tournamentId: number, teamId: number): Promise<Result<void>>;
 }
