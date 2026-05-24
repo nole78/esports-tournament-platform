c368b93abd7b6c540a4dd6ec1bce598fe8bd569e
David Novaković <daki.nole@gmail.com>
2026-05-24 17:42:55 +0200

    Implemented api service for match controller

---
+++ client/src/api_services/matches/IMatchAPIService.ts
@@ -0,0 +1,14 @@
+import type { MatchDto } from "../../models/match/MatchDto";
+import type { MatchResultDto } from "../../models/match/MatchResultDto";
+import type { AddPlayersDto } from "../../models/match_player/AddPlayerDto";
+import type { AddPlayersResponseDto } from "../../models/match_player/AddPlayerResponseDto";
+import type { ApiResponse } from "../../types/api/ApiResponse";
+
+export interface IMatchAPIService {
+    getAllForTorunament(tournamentId: number) : Promise<ApiResponse<MatchDto[]>>;
+    getDetails(id: number) : Promise<ApiResponse<MatchDto>>;
+    setResult(id: number, payload: MatchResultDto) : Promise<ApiResponse<void>>;
+    addPlayers(id: number, payload: AddPlayersDto) : Promise<ApiResponse<AddPlayersResponseDto>>;
+    removePlayer(id: number, userId: number) : Promise<ApiResponse<void>>;
+    changePerformanceNotes(id: number, userId: number, payload: string) : Promise<ApiResponse<void>>;
+}
