7b6867283b70ed456404a50238b0dabfcdb3e300
David Novaković <daki.nole@gmail.com>
2026-05-24 02:59:01 +0200

    Added findByIds to user repo

--- server/src/Domain/repositories/users/IUserRepository.ts
+++ server/src/Domain/repositories/users/IUserRepository.ts
@@ -2,6 +2,7 @@ import { User } from "../../models/User";
 
 export interface IUserRepository {
   findById(id: number): Promise<User>;
+  findByIds(ids: number[]): Promise<User[]>;
   findByUsername(username: string): Promise<User>;
   findByEmail(email: string): Promise<User>;
   findAll(): Promise<User[]>;
