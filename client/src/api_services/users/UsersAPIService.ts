5c3815e3bc1bc2f02e352726bbbf96266484d848
Duro375 <denisdjuris@gmail.com>
2026-05-25 13:48:19 +0200

    Minor fix for role change

--- client/src/api_services/users/UsersAPIService.ts
+++ client/src/api_services/users/UsersAPIService.ts
@@ -25,7 +25,7 @@ export const usersApi: IUsersAPIService = {
       .then(r => r.data).catch(e => err(e, "Failed to load user"));
   },
   async changeRole(id, role) {
-    return axios.put<ApiResponse<void>>(`${BASE}/${id}/role`, {role : role.toUpperCase()}, { headers: authHeader() })
+    return axios.put<ApiResponse<void>>(`${BASE}/${id}/role`, {role : role.toLowerCase()}, { headers: authHeader() })
       .then(r => r.data).catch(e => err(e, "Failed to change user role"));
   },
 };
