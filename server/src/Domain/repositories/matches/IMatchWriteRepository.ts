ce5680d4b346a5f28aa4f18202d624bec2375e10
David Novaković <daki.nole@gmail.com>
2026-05-26 02:40:30 +0200

    added createBulk to MatchWriteRepo

--- server/src/Domain/repositories/matches/IMatchWriteRepository.ts
+++ server/src/Domain/repositories/matches/IMatchWriteRepository.ts
@@ -1,7 +1,9 @@
 import { Match } from "../../models/Match";
+import { MatchRelationUpdate } from "../../types/MatchRelationUpdate";
 
 export interface IMatchWriteRepository {
   create(dto: Match): Promise<Match>;
+  createBulk(matches: Match[]):Promise<Match[]>;
   update(id: number, fields: Partial<Match>): Promise<boolean>;
   delete(id: number): Promise<boolean>;
 }
