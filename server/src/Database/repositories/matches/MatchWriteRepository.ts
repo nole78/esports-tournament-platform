244475f6310fbb21201c18ebbb325b739384e7da
David Novaković <daki.nole@gmail.com>
2026-05-26 03:52:07 +0200

    removed blue and red team from create

--- server/src/Database/repositories/matches/MatchWriteRepository.ts
+++ server/src/Database/repositories/matches/MatchWriteRepository.ts
@@ -15,14 +15,12 @@ export class MatchWriteRepository implements  IMatchWriteRepository{
     const res = await this.db.getWriteConnection();
     if (!res) return [];
     try {
-      const placeholder = matches.map(() => "(?, ?, ?, ?, ?, ?, ?, ?)").join(",");
-      const query = `INSERT INTO matches (tournament_id, blue_team_id, red_team_id,
-          status, round_number, bracket_type, blue_team_score, red_team_score)
+      const placeholder = matches.map(() => "(?, ?, ?, ?, ?, ?)").join(",");
+      const query = `INSERT INTO matches (tournament_id, status, round_number, 
+          bracket_type, blue_team_score, red_team_score)
           VALUES ${placeholder}`;
       const values = matches.flatMap(match => [ 
         match.tournamentId, 
-        match.blueTeamId, 
-        match.redTeamId, 
         match.status,
         match.roundNumber,
         match.bracketType,
