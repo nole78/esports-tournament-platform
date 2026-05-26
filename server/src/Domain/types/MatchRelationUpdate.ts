7b472fefd5eb0a3fdeafb84f50fd84ac4e35bbdc
David Novaković <daki.nole@gmail.com>
2026-05-26 03:51:55 +0200

    Added blue and red team ids to update

--- server/src/Domain/types/MatchRelationUpdate.ts
+++ server/src/Domain/types/MatchRelationUpdate.ts
@@ -1,8 +1,10 @@
 import { MatchSlot } from "../enums/MatchSlot";
 
 export type MatchRelationUpdate = {
-
     matchId: number;
+
+    blueTeamId?: number;
+    redTeamId?: number;
 
     winnerToMatchId?: number;
     winnerToSlot?: MatchSlot;
