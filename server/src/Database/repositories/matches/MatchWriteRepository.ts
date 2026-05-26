f29d2d950fcb3b1d9c22a40a598c0d449d2abf4e
David Novaković <daki.nole@gmail.com>
2026-05-26 03:32:32 +0200

    removed create from MatchWriteRepo (unnecesary)

--- server/src/Database/repositories/matches/MatchWriteRepository.ts
+++ server/src/Database/repositories/matches/MatchWriteRepository.ts
@@ -54,48 +54,6 @@ export class MatchWriteRepository implements  IMatchWriteRepository{
     } finally { res.conn.release(); }
   }
 
-  async create(match: Match): Promise<Match> {
-    const res = await this.db.getWriteConnection();
-    if (!res) return new Match;
-    try {
-      const [result] = await res.conn.execute<ResultSetHeader>(
-        `INSERT INTO matches (tournament_id, blue_team_id, red_team_id,
-        status, round_number, bracket_type, blue_team_score, red_team_score,
-        winner_to_match_id, winner_to_slot, loser_to_match_id, loser_to_slot) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-        [ match.tournamentId, 
-          match.blueTeamId, 
-          match.redTeamId, 
-          match.status,
-          match.roundNumber,
-          match.bracketType,
-          match.blueTeamScore,
-          match.redTeamScore,
-          match.winnerToMatchId,
-          match.winnerToSlot,
-          match.loserToMatchId,
-          match.loserToSlot ]
-      );
-      if (result.insertId === 0) return new Match;
-      return new Match(result.insertId, match.tournamentId, 
-                                        match.blueTeamId, 
-                                        match.redTeamId, 
-                                        match.winnerTeamId, 
-                                        match.status,
-                                        match.roundNumber,
-                                        match.bracketType,
-                                        match.blueTeamScore,
-                                        match.redTeamScore,
-                                        match.winnerToMatchId,
-                                        match.winnerToSlot,
-                                        match.loserToMatchId,
-                                        match.loserToSlot );
-    } catch (err) {
-      this.logger.error("MatchRepository", "create failed", err);
-      return new Match;
-    } finally { res.conn.release(); }
-  }
-
   async update(id: number, fields: Partial<Match>): Promise<boolean> {
     const res = await this.db.getWriteConnection();
     if (!res) return false;
--- server/src/Domain/repositories/matches/IMatchWriteRepository.ts
+++ server/src/Domain/repositories/matches/IMatchWriteRepository.ts
@@ -1,8 +1,6 @@
 import { Match } from "../../models/Match";
-import { MatchRelationUpdate } from "../../types/MatchRelationUpdate";
 
 export interface IMatchWriteRepository {
-  create(dto: Match): Promise<Match>;
   createBulk(matches: Match[]):Promise<Match[]>;
   update(id: number, fields: Partial<Match>): Promise<boolean>;
   delete(id: number): Promise<boolean>;
